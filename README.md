5d619a82bc54ae956aabda67d112abcdb9944885
Amit Danin <amitd@getsanta.com>
2022-12-11 10:30:19 +0200

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+To create AWS Token:
+go to https://d-90676bfcda.awsapps.com/start#/ and click "Command line or programmatic access"
+copy environment variables to your bashrc/zshrc file
--- src/app2.js
+++ src/app2.js
@@ -1,10 +1,21 @@
+require('dotenv').config()
 const express = require("express");
+const AWS = require("aws-sdk");
 
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 const app = express();
 const port = process.env.PORT_SECOND || 3001;
 
 app.get("/s3", async (req, res) => {
-  res.send("s3");
+  const response = await s3
+    .getObject({
+      Bucket: process.env.S3_BUCKET || "santa-test-bucket",
+      Key: process.env.S3_FILE || "santa.txt",
+    })
+    .promise();
+  console.log(response);
+  const data = response.Body.toString("utf-8");
+  res.send(data);
 });
 
 app.listen(port, () => {
